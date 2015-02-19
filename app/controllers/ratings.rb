71b604ab24045d776ed2ca7176e9de9c90e42503
Lucas Santos <lucasrsantos1@gmail.com>
2015-02-18 20:04:39 -0500

    Add things that fix what I need

--- app/controllers/ratings.rb
+++ app/controllers/ratings.rb
@@ -1,7 +1,10 @@
-get '/ratings/new' do
+get '/movies/:id/ratings/new' do |id|
+  @movie = Movie.find(id)
   erb :"ratings/_rating_new", layout: false
 end
 
-post '/ratings' do
-  @rating = Rating.create(user_id: current_user.id, ratable_id: params[:movie][:id], ratable_type: "Movie", rating: params[:rating][:rating])
+post '/movies/:id/ratings' do |id|
+  @movie = Movie.find(id)
+  p params[:rating]
+  @rating = Rating.create(user_id: current_user.id, ratable_id: @movie.id, ratable_type: "Movie", rating: params[:rating].to_i)
 end
--- public/js/ratings.js
+++ public/js/ratings.js
@@ -1,28 +1,30 @@
-// $(document).ready(function () {
-//   $('#options').on("click", "#add-rating", function (e) {
-//     e.preventDefault();
-//     var target = $(this);
-//     var url = target.attr('href');
-//     $.ajax({
-//       type: 'get',
-//       url: url,
-//       success: function (response) {
-//         $('#add-rating').after(response);
-//       }
-//     });
-//   });
+$(document).ready(function () {
+  $('#options').on("click", "#add-rating", function (e) {
+    e.preventDefault();
+    var target = $(this);
+    var url = target.attr('href');
+    $.ajax({
+      type: 'get',
+      url: url,
+      success: function (response) {
+        $('#add-rating').after(response);
+      }
+    });
+  });
 
-//   $('#options').on("click", "#rating-form", function (e) {
-//     e.preventDefault();
-//     var target = $(this);
-//     var url = target.attr('action');
-//     $.ajax({
-//       type: 'post',
-//       url: url,
-//       data: {'movie[id]': , 'rating[:rating]': getInput.input(target, 'input', 0)}
-//       success: function (response) {
-//         $('#add-rating').after(response);
-//       }
-//     });
-//   });
-// });
+  $('#options').on("submit", "#rating-form", function (e) {
+    e.preventDefault();
+    var target = $(this);
+    var url = target.attr('action');
+    var movie_id = $(target.find('input')[0]).attr('name');
+    console.log(getInput.input(target, 'input', 1));
+    $.ajax({
+      type: 'post',
+      url: "/movies/" + movie_id + "/ratings",
+      data: {'movie[id]':  movie_id, 'rating': getInput.input(target, 'input', 1)},
+      success: function (response) {
+        $('#add-rating').after(response);
+      }
+    });
+  });
+});
